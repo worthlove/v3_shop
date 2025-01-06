567ec5bc681a040df636416146d828bae4f72a3e
GZF <13038085710g@gmail.com>
2025-01-06 13:07:12 +0800

    💡 comment(upload.js): 注释更新修正

--- upload.js
+++ upload.js
@@ -6,13 +6,13 @@ const spinner = ora(chalk.green('测试环境:正在发布到服务器中...'));
 spinner.start();
 
 client.scp(
-  './v3_shop/',
+  './v3_shop/', // 打包产生的文件
   {
-    host: '172.0.0.1',
-    port: 8080,
+    host: '172.0.0.1', // 服务器地址
+    port: 8080, // 服务器端口
     username: 'root', // 服务器账号
     password: '123456', // 服务器密码
-    path: '../../../OrbStack/docker/containers/nginx-test/home/v3_shop/'
+    path: '../../../OrbStack/docker/containers/nginx-test/home/v3_shop/' // nginx镜像中的地址
   },
   (err) => {
     spinner.stop();
